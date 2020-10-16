309ddd4fc1f9028195272adda20649e43f906d30
juliocesardemoraes <jcmcf2012@gmail.com>
2020-10-16 15:13:25 -0300

    Segundo

--- tratandoDados.py
+++ tratandoDados.py
@@ -11,68 +11,39 @@ jason = response.json()
 #Serializando json em dictionary
 y = response.json()
 
-#print(response)
-#print(y["MG"]
 
 
+#Inserindo dados de entrada
 
 #mapa = input("Digite o mapa em que se deseja viajar: \n")
 
 origem = input("Digite a origem da rota: \n")
-#A
+
 destino = input("Digite o destino da rota: \n")
-#D
+
 #autonomia = input("Digite a autonomia do caminhão: \n")
 
-#valordolitro = input("Digite o valor do litro: \n")
-
-#A-B
-#B-D
-
-
-# y["MG"].keys())
-x = y["MG"].keys()
-
-origens = []
-destinos = []
-
 common = []
-
 for rota, km in  y["MG"].items():
     
     if rota.startswith(origem):
-        origens.append(rota)
-        
-        rota = rota.strip()
         common.append(rota)
-        #print(rota +": " +str(km))
-        #common.replaces(origem, "")
-    
+        
     if rota.endswith(destino):
-        destinos.append(rota)
-        
-        rota = rota.strip()
-        common.append(rota)
-        #print(rota +": " +str(km))
-    
-counter = 0
+        if not rota in common:
+            common.append(rota)
+
+rotasPossiveis=[]
 
 for i in common:
+    print(i)
+    if i.startswith(origem) and i.endswith(destino):
+        print(i)
+        print(y["MG"][i])  
+        rotasPossiveis.append(i)
     
-    print(i+ " ")
-    print(y["MG"][i])
+        
+    
 
-
-
-
-'''
-for i in origens:
-    print(y["MG"][i])   
-for i in destinos:
-    print(y["MG"][i]) 
-'''
-
-# print(y[mapa].keys())
-
-# extracted = regex.findall("A", y["SP"])
+    
 
